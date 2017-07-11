3921ad89db0fc41d955b32ec3a0ea98b7a2824d5
iand11 <ian.driscoll.o@gmail.com>
2017-07-11 10:47:58 -0700

    Add link to top of page in header, background color change when scrolling, and hover effects for ian link

--- js/application.js
+++ js/application.js
@@ -7,7 +7,21 @@ $(document).ready(function() {
   skillsButtonListener();
   projectsButtonListener();
   contactButtonListener();
+  scrollListener();
+  ianButtonListener();
 });
+
+var scrollListener = function(){
+  $(window).on("scroll", function(){
+    if($(window).scrollTop() > 100){
+      $(".header").addClass("white")
+      $(".ian").addClass('show')
+    } else {
+      $(".header").removeClass("white")
+      $(".ian").removeClass("show")
+    }
+  })
+}
 
 var aboutButtonListener = function(){
   $(".about-button").on("click", function(){
@@ -32,10 +46,19 @@ var projectsButtonListener = function(){
     }, 1000);
   })
 }
+
 var contactButtonListener = function(){
   $(".contact-button").on("click", function(){
     $('html, body').animate({
       scrollTop: $(".contact").offset().top
+    }, 1000);
+  })
+}
+
+var ianButtonListener = function(){
+  $(".ian").on("click", function(){
+    $('html, body').animate({
+      scrollTop: $(".hero-page").offset().top
     }, 1000);
   })
 }
