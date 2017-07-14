65fc65a368fe16444657790177cdf5f9de0dc27d
iand11 <ian.driscoll.o@gmail.com>
2017-07-14 15:14:37 -0700

    Clean up code in both index.html and application.js

--- js/application.js
+++ js/application.js
@@ -1,9 +1,9 @@
 $(document).ready(function() {
-  $(".1").fadeIn(1800);
-  $(".2").delay(800).fadeIn(1500);
-  $(".3").delay(1600).fadeIn(1500);
-  $(".4").delay(2400).fadeIn(1500);
-  $(".header").delay(2700).fadeIn(1500);
+  $('.1').fadeIn(1800);
+  $('.2').delay(800).fadeIn(1500);
+  $('.3').delay(1600).fadeIn(1500);
+  $('.4').delay(2400).fadeIn(1500);
+  $('.header').delay(2700).fadeIn(1500);
   aboutButtonListener();
   skillsButtonListener();
   projectsButtonListener();
@@ -12,54 +12,69 @@ $(document).ready(function() {
   ianButtonListener();
 });
 
-var scrollListener = function(){
-  $(window).on("scroll", function(){
-    if($(window).scrollTop() > 100){
-      $(".header").addClass("white")
-      $(".ian").addClass('show')
+var scrollListener = function() {
+  $(window).on('scroll', function() {
+    if ($(window).scrollTop() > 100) {
+      $('.header').addClass('white');
+      $('.ian').addClass('show');
     } else {
-      $(".header").removeClass("white")
-      $(".ian").removeClass("show")
+      $('.header').removeClass('white');
+      $('.ian').removeClass('show');
     }
-  })
-}
+  });
+};
 
-var aboutButtonListener = function(){
-  $(".about-button").on("click", function(){
-    $('html, body').animate({
-      scrollTop: $(".about-section").offset().top
-    }, 1000);
-  })
-}
+var aboutButtonListener = function() {
+  $('.about-button').on('click', function() {
+    $('html, body').animate(
+      {
+        scrollTop: $('.about-section').offset().top
+      },
+      1000
+    );
+  });
+};
 
-var skillsButtonListener = function(){
-  $(".skills-button").on("click", function(){
-    $('html, body').animate({
-      scrollTop: $(".skills-section").offset().top
-    }, 1000);
-  })
-}
+var skillsButtonListener = function() {
+  $('.skills-button').on('click', function() {
+    $('html, body').animate(
+      {
+        scrollTop: $('.skills-section').offset().top
+      },
+      1000
+    );
+  });
+};
 
-var projectsButtonListener = function(){
-  $(".projects-button").on("click", function(){
-    $('html, body').animate({
-      scrollTop: $(".projects-section").offset().top
-    }, 1000);
-  })
-}
+var projectsButtonListener = function() {
+  $('.projects-button').on('click', function() {
+    $('html, body').animate(
+      {
+        scrollTop: $('.projects-section').offset().top
+      },
+      1000
+    );
+  });
+};
 
-var contactButtonListener = function(){
-  $(".contact-button").on("click", function(){
-    $('html, body').animate({
-      scrollTop: $(".contact").offset().top
-    }, 1000);
-  })
-}
+var contactButtonListener = function() {
+  $('.contact-button').on('click', function() {
+    $('html, body').animate(
+      {
+        scrollTop: $('.contact').offset().top
+      },
+      1000
+    );
+  });
+};
 
-var ianButtonListener = function(){
-  $(".ian").on("click", function(){
-    $('html, body').animate({
-      scrollTop: $(".hero-page").offset().top
-    }, 1000);
-  })
-}
+var ianButtonListener = function() {
+  $('.ian').on('click', function() {
+    $('html, body').animate(
+      {
+        scrollTop: $('.hero-page').offset().top
+      },
+      1000
+    );
+  });
+};
