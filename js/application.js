f7a61bd4041f5c2431cd743dc55e7f226ab49fdb
Ian Driscoll <ian.driscoll.o@gmail.com>
2017-10-21 22:40:45 -0700

    Add responsive design

--- js/application.js
+++ js/application.js
@@ -12,23 +12,51 @@ $(document).ready(function() {
   ianButtonListener();
 });
 
+
 var scrollListener = function() {
+  const about = $('.about-section').offset().top;
+  const skills = $('.skills-section').offset().top;
+  const projects = $('.projects-section').offset().top;
+  const contact = $('.contact').offset().top;
+
   $(window).on('scroll', function() {
-    if ($(window).scrollTop() > 100) {
+    const scroll = $(window).scrollTop();
+    if (scroll > 100) {
       $('.header').addClass('white');
       $('.ian').addClass('show');
+      $('.links').show();
     } else {
       $('.header').removeClass('white');
       $('.ian').removeClass('show');
+      $('.links').hide();
+    }
+    if(scroll > about - 60 && scroll < skills - 60){
+      $('.about-button').addClass('clicked');
+    } else {
+      $('.about-button').removeClass('clicked');
+    }
+    if(scroll > skills - 60 && scroll < projects - 60){
+      $('.skills-button').addClass('clicked');
+    } else {
+      $('.skills-button').removeClass('clicked');
+    }
+    if(scroll > projects - 60 && scroll < contact - 50){
+      $('.projects-button').addClass('clicked');
+    } else {
+      $('.projects-button').removeClass('clicked');
     }
   });
+
+  // if(scrollPoint > about && scrollPoint < skills){
+  // } else {
+  //   $('.about-button').removeClass('clicked');
+  // }
 };
 
 var aboutButtonListener = function() {
   $('.about-button').on('click', function() {
-    $('html, body').animate(
-      {
-        scrollTop: $('.about-section').offset().top
+    $('html, body').animate({
+        scrollTop: $('.about-section').offset().top - 50
       },
       1000
     );
@@ -37,9 +65,8 @@ var aboutButtonListener = function() {
 
 var skillsButtonListener = function() {
   $('.skills-button').on('click', function() {
-    $('html, body').animate(
-      {
-        scrollTop: $('.skills-section').offset().top
+    $('html, body').animate({
+        scrollTop: $('.skills-section').offset().top - 50
       },
       1000
     );
@@ -48,9 +75,8 @@ var skillsButtonListener = function() {
 
 var projectsButtonListener = function() {
   $('.projects-button').on('click', function() {
-    $('html, body').animate(
-      {
-        scrollTop: $('.projects-section').offset().top
+    $('html, body').animate({
+        scrollTop: $('.projects-section').offset().top - 50
       },
       1000
     );
@@ -59,9 +85,8 @@ var projectsButtonListener = function() {
 
 var contactButtonListener = function() {
   $('.contact-button').on('click', function() {
-    $('html, body').animate(
-      {
-        scrollTop: $('.contact').offset().top
+    $('html, body').animate({
+        scrollTop: $('.contact').offset().top - 50
       },
       1000
     );
@@ -70,8 +95,7 @@ var contactButtonListener = function() {
 
 var ianButtonListener = function() {
   $('.ian').on('click', function() {
-    $('html, body').animate(
-      {
+    $('html, body').animate({
         scrollTop: $('.hero-page').offset().top
       },
       1000
